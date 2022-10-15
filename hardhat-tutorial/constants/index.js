6afeb2e1637fdb1b2d941387bc00e76e558d2549
Bakaka <yawudima2016@gmail.com>
2022-10-15 13:51:55 -0700

    HHHHHHH

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x5cc15a9592264688B4DE5E5799628e826B50243f";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
